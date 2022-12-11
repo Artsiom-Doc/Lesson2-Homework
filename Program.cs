aabd83b7f9d857dff7299b4406d134734ba8d140
Artsiom <Frt@mail.ru>
2022-12-11 12:15:10 +0300

    математический вариант нахождения 2-ой цифры трёхзначного числа

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите трехзначное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int Digit = N / 10;
+int secondDigit = Digit % 10;
+Console.WriteLine(secondDigit);
